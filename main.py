637084b0a0e2f8c9e565c10367933c4c703dcb4b
Chris Tuncan <christopher.tuncan@flir.com>
2021-08-30 19:24:11 +0100

    [Python] 2015 day 2
    Technically answers are accepted, however I'm not particularly happy
    with the code! Certainly not production quality!

--- main.py
+++ main.py
@@ -15,3 +15,27 @@ def aoc2015_1():
         if floor == -1 and first_basement is None:
             first_basement = i + 1
     return floor, first_basement
+
+def aoc2015_2():
+    def single_present(dimensions):
+        print (dimensions)
+        l, w, h = map(int, dimensions.split('x'))
+        area_sides = (l*w, w*h, h*l)
+        perimeter_sides = (l+w, w+h, h+l)
+        smallest_perimeter = 2*min(perimeter_sides)
+        volume = l*w*h
+        ribbon = smallest_perimeter + volume
+        smallest_side = min(area_sides)
+        surface_area = 2*sum(area_sides) + smallest_side
+        print(surface_area, ribbon)
+        return surface_area, ribbon
+    assert(single_present("2x3x4")== (58, 34))
+    assert(single_present("1x1x10") == (43,14))
+    input = open("2015/02/input")
+    total_surface_area = 0
+    total_ribbon = 0
+    for present in input:
+        surface_area, ribbon = single_present(present)
+        total_surface_area += surface_area
+        total_ribbon += ribbon
+    return total_surface_area, total_ribbon
